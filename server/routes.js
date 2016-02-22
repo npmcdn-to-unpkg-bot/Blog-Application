c93c41dcc40ace7a2b9207c7cd51df414ff9b305
Ayeni Olusegun <nsegun5@gmail.com>
2016-02-22 07:28:55 +0100

    Add Token Middleware

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,5 @@
 var userController = require('./controllers/user.controller');
 module.exports = function(app){
-
-    app.post('/api/register', userController.registerUser);
+    app.post('/api/register', userController.tokenMiddleware, userController.registerUser);
     app.post('/api/login', userController.authenticateUser);
 }
