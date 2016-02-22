01a3227ab92d56c870c57000363e28043fdbf49b
Ayeni Olusegun <nsegun5@gmail.com>
2016-02-23 00:54:20 +0100

    Refactor Model Name

--- server/models/comment.js
+++ server/models/comment.js
@@ -8,7 +8,7 @@ var commentSchema =new schema({
     datePublished : {type : Date, default : Date.now},
     content : {type : String},
     author : {type : String},
-    blogId : {type : schema.ObjectId, ref : 'posts'}
+    blogId : {type : schema.ObjectId, ref : 'post'}
 });
 
 module.exports = mongoose.model('comment', commentSchema, 'comments');
