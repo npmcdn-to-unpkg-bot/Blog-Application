d4aace4c9f20018dff44be93072923494a8203bd
Ayeni Olusegun <nsegun5@gmail.com>
2016-02-25 07:31:37 +0100

    Add config and view to admin module

--- public/modules/admin/adminModule.js
+++ public/modules/admin/adminModule.js
@@ -1,3 +1,10 @@
 /**
  * Created by Raphson on 2/25/16.
  */
+angular.module('blogger.admin', []).config(['$stateProvider', function($stateProvider){
+    $stateProvider.state('admin',{
+        url: '/admin',
+        controller: 'AdminController',
+        templateUrl: ''
+    })
+}]);
