2af9a9f415e163c84c6bfbea9c4edd12ba70c28d
Ayeni Olusegun <nsegun5@gmail.com>
2016-02-25 07:42:17 +0100

    Add Route Config to admin

--- public/modules/admin/adminModule.js
+++ public/modules/admin/adminModule.js
@@ -4,7 +4,24 @@
 angular.module('blogger.admin', []).config(['$stateProvider', function($stateProvider){
     $stateProvider.state('admin',{
         url: '/admin',
+        abstract: true,
         controller: 'AdminController',
-        templateUrl: ''
-    })
+        templateUrl: 'modules/admin/views/admin-home.html'
+    });
+    $stateProvider.state('admin.postNew', {
+        url: '/posts/new',
+        controller: 'postCreationController',
+        templateUrl: 'modules/admin/views/admin-new-post.html'
+    });
+    $stateProvider.state('admin.postUpdate', {
+        url: '/posts/:id/edit',
+        controller: 'postUpdateController',
+        templateUrl: 'modules/admin/views/admin-update-post.html'
+    });
+    $stateProvider.state('admin.postViewAll', {
+        url: '',
+        controller: 'postListController',
+        templateUrl: 'modules/admin/views/admin-all-posts.html'
+    });
+
 }]);
