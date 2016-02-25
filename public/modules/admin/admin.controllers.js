7726ff9118eb05211b71ca030127f8f6a9ef86d9
Ayeni Olusegun <nsegun5@gmail.com>
2016-02-25 21:44:52 +0100

    HTML view for admin-home

--- public/modules/admin/admin.controllers.js
+++ public/modules/admin/admin.controllers.js
@@ -1,3 +1,10 @@
-/**
- * Created by Raphson on 2/25/16.
- */
+angular.module('blogger.admin.controller', [])
+    .cotroller('AdminController', ['$scope', function($scope){
+
+    }]).controller('postCreationController', ['$scope', function($scope){
+
+    }]).controller('postUpdateController', ['$scope', function($scope){
+
+    }]).controller('postListController', ['$scope', function($scope){
+
+    }]);
