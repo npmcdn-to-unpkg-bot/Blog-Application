b6365eb0ce73d93e55842c3b2da6df948c6f8083
Ayeni Olusegun <nsegun5@gmail.com>
2016-03-02 07:11:54 +0100

    Fix bug

--- public/modules/posts/post.directives.js
+++ public/modules/posts/post.directives.js
@@ -1,8 +1,8 @@
 /**
  * Created by Raphson on 3/1/16.
  */
-/*angular.module('blogger.posts.directives', [])
-    .directive('spComments', [function(){
+angular.module('blogger.posts.directives', [])
+    .directive('spbComments', [function(){
        return {
            restrict : 'AEC',
            scope : {
@@ -10,26 +10,11 @@
            },
            replace : true,
            link: function(scope, elem, attrs){
-               
+
            },
            templateUrl : 'modules/posts/views/comments.html'
        }
-    }]);*/
+    }]);
 
 
-angular.module('blogger.posts.directives',[]).directive('spbComments',[function(){
-    return {
-        restrict:'AEC',
 
-        scope:{
-            postInstance:'='
-        },
-        replace:true,
-
-        link:function(scope,elem,attrs){
-
-
-        },
-        templateUrl:'modules/posts/views/comments.html'
-    }
-}]);
